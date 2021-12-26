d2f14b53fc78521317f7889c8ba5f3f7338b0a8f
Vova Moskalyuk <vova.moskalyuk11jr@gmail.com>
2021-12-26 15:52:29 +0200

    task 6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,10 +1,10 @@
 const input = document.querySelector("#validation-input");
-const validNumberOfCharacters = input.dataset.length;
+const validNumberOfCharacters = Number(input.dataset.length);
 
 input.addEventListener("blur", (evt) => {
   const currentValue = evt.currentTarget.value.trim();
 
-  currentValue.length >= validNumberOfCharacters
+  currentValue.length === validNumberOfCharacters
     ? (input.classList.remove("invalid"), input.classList.add("valid"))
     : (input.classList.remove("valid"), input.classList.add("invalid"));
 });
